d344d8dc664a368fce15400f799fb241fa6f766a
Far2Casual <63196064+Far2Casual@users.noreply.github.com>
2020-05-03 14:11:53 +0200

    Implémentation de l'Attaque de groupe du compagnon (#74)
    * Ajout d'une option "attaqueEnGroupe" sur les attaques pour supporter la règle du Compagnon
    
    * Renommage Attaque DE groupe
    
    * Renommage Attaque DE groupe

--- COFantasy.js
+++ COFantasy.js
@@ -3445,6 +3445,7 @@ var COFantasy = COFantasy || function() {
           scope[cmd[0]] += bAtt;
           return;
         case "bonusCritique":
+        case "attaqueDeGroupe":
           if (cmd.length < 2) {
             error("Usage : --" + cmd[0] + " b", cmd);
             return;
@@ -4893,6 +4894,11 @@ var COFantasy = COFantasy || function() {
       attBonus += 5;
       explications.push("Attaque assurée => +5 en Attaque et DM/2");
     }
+    if (options.attaqueDeGroupe > 1) {
+      var bonusTouche = 2 * (options.attaqueDeGroupe - 1);
+      attBonus += bonusTouche;
+      explications.push("Attaque en groupe => +" + bonusTouche + " en Attaque");
+    }
     if (attributeAsBool(attaquant, 'criDuPredateur')) {
       attBonus += 1;
       explications.push("Cri du prédateur => +1 en attaque");
@@ -6065,6 +6071,10 @@ var COFantasy = COFantasy || function() {
       return 0;
     }
     var dmgCoef = options.dmgCoef || 1;
+    if(options.attaqueDeGroupeDmgCoef) {
+      dmgCoef++;
+      expliquer("Attaque en groupe > DEF +5 => DMGx" + (crit ? "3" : "2"));
+    }
     if (target.dmgCoef) dmgCoef += target.dmgCoef;
     var critCoef = 1;
     if (crit) {
@@ -6721,6 +6731,9 @@ var COFantasy = COFantasy || function() {
               setState(target, 'renverse', true, evt);
               target.messages.push("tombe par terre");
             }
+          }
+          if (options.attaqueDeGroupe > 1 && attackRoll >= (defense + 5)) {
+            options.attaqueDeGroupeDmgCoef = true;
           }
           if (d20roll == 1 && options.chance === undefined) {
             attackResult = " : <span style='" + BS_LABEL + " " + BS_LABEL_DANGER + "'><b>échec&nbsp;critique</b></span>";
