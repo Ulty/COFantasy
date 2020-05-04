ef513e45a4355402505136b59d2d2b9f1f210432
Far2Casual <63196064+Far2Casual@users.noreply.github.com>
2020-05-04 09:59:21 +0200

    Option de génération automatique de bouton d'attaque de groupe (#75)
    * Ajout d'une option permettant d'ajouter automatiquement des options d'attaques (normales, assurée, risquée) aux boutons d'attaques de corps-à-corps et à distance générés par !cof-liste-actions
    
    * Ajout d'une option permettant d'ajouter automatiquement des options d'attaques en groupe aux PNJ
    
    * Ajout d'une option "attaqueEnGroupe" sur les attaques pour supporter la règle du Compagnon
    
    * Renommage Attaque DE groupe
    
    * Renommage Attaque DE groupe
    
    * Renommage Attaque DE groupe

--- COFantasy.js
+++ COFantasy.js
@@ -96,6 +96,11 @@ var COFantasy = COFantasy || function() {
           val: false,
           type: 'bool'
         },
+        generer_attaque_groupe: {
+          explications: "Ajouter automatiquement une option 'Attaque de groupe' (cf. Compagnon p.99) pour les PNJ",
+          val: false,
+          type: 'bool'
+        },
         mana_totale: {
           explications: "Tous les sorts ont un coût, celui des tempêtes de mana est multiplié par 3",
           val: false,
@@ -464,6 +469,7 @@ var COFantasy = COFantasy || function() {
     var style = '';
     var picto = '';
     var options;
+    var groupe;
     var command = fullCommand.split(' ');
     // Pictos : https://wiki.roll20.net/CSS_Wizardry#Pictos
     switch (command[0]) {
@@ -499,14 +505,17 @@ var COFantasy = COFantasy || function() {
           picto = '<span style="font-family: \'Pictos Custom\'">[</span> ';
           style = 'background-color:#48b92c';
           if (stateCOF.options.regles.val.generer_options_attaques.val) {
-            options = "?{Type d'Attaque|Normale,&#32;|Assurée,--attaqueAssuree}";
+            options = "?{Type d'Attaque|Normale,|Assurée,--attaqueAssuree}";
           }
         } else {
           // attaque contact
           picto = '<span style="font-family: \'Pictos Custom\'">t</span> ';
           style = 'background-color:#cc0000';
           if (stateCOF.options.regles.val.generer_options_attaques.val) {
-            options = "?{Type d'Attaque|Normale,&#32;|Assurée,--attaqueAssuree|Risquée,--attaqueRisquee}";
+            options = "?{Type d'Attaque|Normale,|Assurée,--attaqueAssuree|Risquée,--attaqueRisquee}";
+          }
+          if (stateCOF.options.regles.val.generer_attaque_groupe.val && perso.pnj && perso.token.get('bar1_link') === "") {
+            groupe = true;
           }
         }
         break;
@@ -582,7 +591,8 @@ var COFantasy = COFantasy || function() {
     return {
       picto: picto,
       style: style,
-      options: options
+      options: options,
+      groupe: groupe
     };
   }
 
@@ -1696,9 +1706,16 @@ var COFantasy = COFantasy || function() {
       buttonStyle = ' style="' + optionsFromCommand.style + '"';
     if (overlay) overlay = ' title="' + overlay + '"';
     else overlay = '';
-    if (optionsFromCommand.options)
+
+    var baseAction = action;
+    if (optionsFromCommand.options) {
       action += ' ' + optionsFromCommand.options;
-    return boutonSimple(action, buttonStyle + overlay, text);
+    }
+    var toReturn = boutonSimple(action, buttonStyle + overlay, text);
+    if (optionsFromCommand.groupe) {
+      toReturn += "<br/>" + boutonSimple(baseAction + " --attaqueDeGroupe ?{Attaque en groupe ?}", buttonStyle + overlay, text + " (groupe)");
+    }
+    return toReturn ;
   }
 
   function improve_image(image_url) {
